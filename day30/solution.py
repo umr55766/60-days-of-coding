c147e32ed1e1b905028c7bc0b8eca926e3a0f67a
Umair <umr55766@gmail.com>
2020-07-29 01:57:11 +0530

    Max value in array post op

--- day30/solution.py
+++ day30/solution.py
@@ -19,10 +19,80 @@ def jump(cloudSafetyIndexes, currentPosition, stepsTaken):
 
     return stepsTaken
 
+
 def repeatedString(s, n):
     countOfAsInString = len([1 for char in s if char == "a"])
 
-    totalAsCount = (n//len(s)) * countOfAsInString
-    totalAsCount += len([1 for char in s[0: (n%len(s))] if char == "a"])
+    totalAsCount = (n // len(s)) * countOfAsInString
+    totalAsCount += len([1 for char in s[0: (n % len(s))] if char == "a"])
 
     return totalAsCount
+
+
+def minimumBribes(q):
+    swapCount = 0
+
+    for index, number in enumerate(q):
+        if number - index > 3:
+            return "Too chaotic"
+
+    for i in range(len(q) - 1, 0, -1):
+        if q[i] != i+1:
+            if q[i - 1] == i+1:
+                q[i - 1], q[i] = q[i], q[i - 1]
+                swapCount += 1
+            else:
+                q[i - 2], q[i - 1] = q[i - 1], q[i - 2]
+                q[i - 1], q[i] = q[i], q[i - 1]
+                swapCount += 2
+
+    return swapCount
+
+
+def minimumSwaps(arr):
+    swapCount = 0
+    valueIndexes = {}
+
+    for index, value in enumerate(arr):
+        valueIndexes[value] = index
+
+    for index in range(0, len(arr)):
+        if arr[index] != index+1:
+            sourceValue = arr[index]
+            destinationValue = index+1
+
+            sourceIndex = index
+            destinationIndex = valueIndexes[destinationValue]
+
+            arr[sourceIndex], arr[destinationIndex] = arr[destinationIndex], arr[sourceIndex]
+            valueIndexes[sourceValue] = destinationIndex
+            valueIndexes[destinationValue] = sourceIndex
+
+            swapCount += 1
+
+    return swapCount
+
+
+def arrayManipulation(n, queries):
+    values = {}
+
+    for [a, b, k] in queries:
+        if a in values:
+            values[a] += k
+        else:
+            values[a] = k
+
+        if b + 1 in values:
+            values[b + 1] -= k
+        else:
+            values[b + 1] = -k
+
+    indexes = sorted([key for key in values])
+
+    maximum = 0
+    sumUntilNow = 0
+    for index in indexes:
+        maximum = max(maximum, sumUntilNow + values[index])
+        sumUntilNow += values[index]
+
+    return maximum
--- day30/tests.py
+++ day30/tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from day30.solution import jumpingOnClouds, repeatedString
+from day30.solution import jumpingOnClouds, repeatedString, minimumBribes, minimumSwaps, arrayManipulation
 
 
 class Test(unittest.TestCase):
@@ -11,7 +11,28 @@ class Test(unittest.TestCase):
         self.assertEqual(3, jumpingOnClouds([0, 0, 0, 0, 1, 0]))
 
     def test_3(self):
-        self.assertEqual(7, repeatedString("aba",10))
+        self.assertEqual(7, repeatedString("aba", 10))
 
     def test_4(self):
         self.assertEqual(1000000000000, repeatedString("a", 1000000000000))
+
+    def test_5(self):
+        self.assertEqual(3, minimumBribes([2, 1, 5, 3, 4]))
+
+    def test_6(self):
+        self.assertEqual("Too chaotic", minimumBribes([2, 5, 1, 3, 4]))
+
+    def test_7(self):
+        self.assertEqual(3, minimumSwaps([4, 3, 1, 2]))
+
+    def test_8(self):
+        self.assertEqual(3, minimumSwaps([2, 3, 4, 1, 5]))
+
+    def test_9(self):
+        self.assertEqual(3, minimumSwaps([1, 3, 5, 2, 4, 6, 7]))
+
+    def test_10(self):
+        self.assertEqual(31, arrayManipulation(10, [[2, 6, 8],
+                                                 [3, 5, 7],
+                                                 [1, 8, 1],
+                                                 [5, 9, 15]]))
