3b398b823a80df13c22c1a2f8f650e6f344c6050
mohammadumair <umr55766@gmail.com>
2024-10-16 15:08:40 +0400

    Add implementation

--- journey_2/day1/solution.py
+++ journey_2/day1/solution.py
@@ -3,4 +3,13 @@ class MergeStringAlternatively:
         pass
 
     def merge(self, string1, string2):
-        pass
+        result = ""
+        index = 0
+        while index < min(len(string1), len(string2)):
+            result += string1[index] + string2[index]
+            index += 1
+
+        result += string1[index:]
+        result += string2[index:]
+
+        return result
