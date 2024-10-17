260513bbcbff58c569dbf3be24f2c02d9d152fa1
mohammadumair <umr55766@gmail.com>
2024-10-17 19:25:40 +0400

    fix

--- journey_2/day2/day2_2/solution.py
+++ journey_2/day2/day2_2/solution.py
@@ -1,7 +1,19 @@
+from journey_1.day5.solution import start_index
+
+
 class Solution:
     def __init__(self):
         pass
 
     def method_name(self, array, length):
-        max_sum = sum(array[:length])
-        return max_sum
+        running_sum = max_sum = sum(array[:length])
+        front_index = 0
+        rear_index = length
+
+        while rear_index < len(array):
+            running_sum += array[rear_index] - array[front_index]
+            max_sum = max(max_sum, running_sum)
+            rear_index += 1
+            front_index += 1
+
+        return max_sum / length
