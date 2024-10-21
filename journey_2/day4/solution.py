5256852f34ba227117ec318e05194b5030adbcfa
mohammadumair <umr55766@gmail.com>
2024-10-21 17:12:52 +0400

    Implement solution

--- journey_2/day4/solution.py
+++ journey_2/day4/solution.py
@@ -2,5 +2,17 @@ class Solution:
     def __init__(self):
         pass
 
-    def method_name(self, param):
-        return param
+    def asteroid_collision(self, asteroids):
+        stack = []
+        for asteroid in asteroids:
+            while stack and asteroid < 0 < stack[-1]:
+                if stack[-1] < -asteroid:
+                    stack.pop()
+                    continue
+                elif stack[-1] == -asteroid:
+                    stack.pop()
+                break
+            else:
+                stack.append(asteroid)
+
+        return stack
