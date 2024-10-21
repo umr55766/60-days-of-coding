58bccd338428d777adfb03b3bdc49cccbcdcfc23
mohammadumair <umr55766@gmail.com>
2024-10-21 17:12:44 +0400

    Add test

--- journey_2/day4/tests.py
+++ journey_2/day4/tests.py
@@ -2,13 +2,23 @@ import unittest
 
 from journey_2.day4.solution import Solution
 
-
 class SolutionTest(unittest.TestCase):
-    def test_1(self):
-        self.assertEqual(Solution().method_name(1), 1)
+    def test_asteroids_case_1(self):
+        asteroids = [5, 10, -5]
+        expected_output = [5, 10]
+        self.assertEqual(Solution().asteroid_collision(asteroids), expected_output)
 
-    def test_2(self):
-        self.assertEqual(Solution().method_name(2), 2)
+    def test_asteroids_case_2(self):
+        asteroids = [8, -8]
+        expected_output = []
+        self.assertEqual(Solution().asteroid_collision(asteroids), expected_output)
 
-    def test_3(self):
-        self.assertEqual(Solution().method_name(3), 3)
+    def test_asteroids_case_3(self):
+        asteroids = [10, 2, -5]
+        expected_output = [10]
+        self.assertEqual(Solution().asteroid_collision(asteroids), expected_output)
+
+    def test_asteroids_case_4(self):
+        asteroids = [-2, -1, 1, 2]
+        expected_output = [-2, -1, 1, 2]
+        self.assertEqual(Solution().asteroid_collision(asteroids), expected_output)
