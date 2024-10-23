4489ac723c4fc8f31cb8db23219e60aa346dab4e
mohammadumair <umr55766@gmail.com>
2024-10-23 16:41:52 +0400

    Add failing test

--- journey_2/day6/tests.py
+++ journey_2/day6/tests.py
@@ -1,14 +1,27 @@
 import unittest
 
-from journey_2.day6.solution import Solution
+from journey_2.day6.solution import Solution, TreeNode
 
 
 class SolutionTest(unittest.TestCase):
     def test_1(self):
-        self.assertEqual(Solution().method_name(1), 1)
+        self.root = TreeNode(4)
+        self.root.left = TreeNode(2)
+        self.root.right = TreeNode(7)
+        self.root.left.left = TreeNode(1)
+        self.root.left.right = TreeNode(3)
+
+        result = Solution().search_bst(self.root, 2)
+        self.assertEqual(result.val, 2)
+        self.assertEqual(result.left.val, 1)
+        self.assertEqual(result.right.val, 3)
 
     def test_2(self):
-        self.assertEqual(Solution().method_name(2), 2)
+        self.root = TreeNode(4)
+        self.root.left = TreeNode(2)
+        self.root.right = TreeNode(7)
+        self.root.left.left = TreeNode(1)
+        self.root.left.right = TreeNode(3)
 
-    def test_3(self):
-        self.assertEqual(Solution().method_name(3), 3)
+        result = Solution().search_bst(self.root, 5)
+        self.assertIsNone(result)
