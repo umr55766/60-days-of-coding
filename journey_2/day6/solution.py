ae97bb6f886af815d704f326376f5f9d2e183a01
mohammadumair <umr55766@gmail.com>
2024-10-23 16:41:58 +0400

    Add sol

--- journey_2/day6/solution.py
+++ journey_2/day6/solution.py
@@ -2,5 +2,18 @@ class Solution:
     def __init__(self):
         pass
 
-    def method_name(self, param):
-        return param
+    def search_bst(self, root, param):
+        if root is None:
+            return None
+
+        if root.val == param:
+            return root
+
+        return self.search_bst(root.left, param) or self.search_bst(root.right, param)
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
