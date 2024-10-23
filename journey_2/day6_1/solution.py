000f74fe74de7767e6c7ff7a22bacd810368f7c1
mohammadumair <umr55766@gmail.com>
2024-10-23 17:35:37 +0400

    Solve 6_1

--- journey_2/day6_1/solution.py
+++ journey_2/day6_1/solution.py
@@ -1,6 +1,26 @@
+from journey_1.day7.solution import index
+
+
 class Solution:
     def __init__(self):
-        pass
+        self.ROOMS = []
+        self.VISITED_ROOMS = []
 
     def can_visit_all_rooms(self, rooms):
-        pass
+        self.VISITED_ROOMS = [False for _ in rooms]
+        self.ROOMS = rooms
+
+        self.VISITED_ROOMS[0] = True
+
+        for index in rooms[0]:
+            if not self.VISITED_ROOMS[index]:
+                self.visit(index)
+
+        return all(self.VISITED_ROOMS)
+
+    def visit(self, visiting_index):
+        self.VISITED_ROOMS[visiting_index] = True
+
+        for to_visit in self.ROOMS[visiting_index]:
+            if not self.VISITED_ROOMS[to_visit]:
+                self.visit(to_visit)
