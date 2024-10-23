3d583850ae9544a17d847a6c66182615c989d333
mohammadumair <umr55766@gmail.com>
2024-10-23 16:51:54 +0400

    Add day 6_1

---
+++ journey_2/day6_1/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def __init__(self):
+        pass
+
+    def method_name(self, param):
+        return param
+
+    def can_visit_all_rooms(self, rooms):
+        pass
---
+++ journey_2/day6_1/tests.py
@@ -0,0 +1,15 @@
+import unittest
+
+from journey_2.day6_1.solution import Solution
+
+
+class SolutionTest(unittest.TestCase):
+    def test_1(self):
+        rooms = [[1], [2], [3], []]
+        result = Solution().can_visit_all_rooms(rooms)
+        self.assertTrue(result)  # Expected output: True
+
+    def test_2(self):
+        rooms = [[1, 3], [3, 0, 1], [2], [0]]
+        result = Solution().can_visit_all_rooms(rooms)
+        self.assertFalse(result)  # Expected output: False
