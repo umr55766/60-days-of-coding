7ac9ee572b96ce6c4e92ad6d5af3fcd19b282ea6
mohammadumair <umr55766@gmail.com>
2024-10-24 19:08:04 +0400

    solve day_7_1

--- journey_2/day7_1/solution.py
+++ journey_2/day7_1/solution.py
@@ -1,6 +1,18 @@
+from heapq import heappush, heappushpop
+
+
 class Solution:
     def __init__(self):
         pass
 
-    def find_kth_largest(self, param, i):
-        return param
+    def find_kth_largest(self, nums, k):
+        heap = []
+
+        for i in range(k):
+            heappush(heap, nums[i])
+
+        for i in range(k, len(nums)):
+            if nums[i] > heap[0]:
+                heappushpop(heap, nums[i])
+
+        return heap[0]
