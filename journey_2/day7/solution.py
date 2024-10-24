b90f3f98783644eef286c52481dc5ca2ef9d5b4a
mohammadumair <umr55766@gmail.com>
2024-10-24 14:03:16 +0400

    Improve solution

--- journey_2/day7/solution.py
+++ journey_2/day7/solution.py
@@ -1,39 +1,34 @@
+from collections import deque
+
+
 class Solution:
-    def __init__(self):
-        self.maze = None
-        self.directions = [
+    def nearest_exit(self, maze, entrance, location = None, distance = None):
+        directions = [
             [0, 1],
             [1, 0],
             [0, -1],
             [-1, 0]
         ]
-        self.result = None
+        maze[entrance[0]][entrance[1]] = "*"
+        queue = deque([[entrance[0], entrance[1], 0]])
 
-    def nearest_exit(self, maze, entrance, location = None, distance = None):
-        if self.maze is None:
-            self.maze = maze
+        while queue:
+            i, j, distance = queue.popleft()
 
-        if location is not None and self.is_at_exit(location):
-            return distance
+            if self.is_at_exit(maze, i, j) and [i, j] != entrance:
+                return distance
 
-        for direction in self.directions:
-            if location is None:
-                self.maze[entrance[0]][entrance[1]] = "*"
-                next_location = [entrance[0] + direction[0], entrance[1] + direction[1]]
-                if self.can_visit(next_location):
-                    temp = self.nearest_exit(maze, entrance, next_location, 1)
-                    self.result = min(self.result, temp) if self.result is not None and self.result!=-1 else temp
-            else:
-                self.maze[location[0]][location[1]] = "*"
-                next_location = [location[0] + direction[0], location[1] + direction[1]]
-                if self.can_visit(next_location):
-                    temp = self.nearest_exit(maze, entrance, next_location, distance+1)
-                    self.result = min(self.result, temp) if self.result is not None and self.result!=-1 else temp
+            for x, y in directions:
+                x, y = x + i, y + j
 
-        return self.result if self.result is not None else -1
+                if self.can_visit(maze, x, y):
+                    maze[x][y] = "*"
+                    queue.append((x, y, distance + 1))
 
-    def is_at_exit(self, entrance):
-        return (entrance[0] == 0 or entrance[0] == len(self.maze)-1) or (entrance[1] == 0 or entrance[1] == len(self.maze[0])-1)
+        return -1
 
-    def can_visit(self, location):
-        return (0 <= location[0] < len(self.maze)) and (0 <= location[1] < len(self.maze[0])) and self.maze[location[0]][location[1]] == "."
+    def is_at_exit(self, maze, x, y):
+        return (x in (0, len(maze) - 1) or y in (0, len(maze[0]) - 1))
+
+    def can_visit(self, maze, x, y):
+        return x in range(0, len(maze)) and y in range(0, len(maze[0])) and maze[x][y] == "."
