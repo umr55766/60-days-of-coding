5b695331383fa2d855cd6364cf0c7b342d299afa
mohammadumair <umr55766@gmail.com>
2024-10-24 13:11:00 +0400

    Fix 1 test

--- journey_2/day7/solution.py
+++ journey_2/day7/solution.py
@@ -1,3 +1,37 @@
 class Solution:
-    def nearest_exit(self, maze, entrance):
-        pass
+    def __init__(self):
+        self.maze = None
+        self.directions = [
+            [0, 1],
+            [1, 0],
+            [0, -1],
+            [-1, 0]
+        ]
+        self.result = None
+
+    def nearest_exit(self, maze, entrance, location = None, distance = None):
+        if self.maze is None:
+            self.maze = maze
+
+        if location is not None and self.is_at_exit(location):
+            return distance
+
+        for direction in self.directions:
+            if location is None:
+                next_location = [entrance[0] + direction[0], entrance[1] + direction[1]]
+                if self.can_visit(next_location):
+                    temp = self.nearest_exit(maze, entrance, next_location, 1)
+                    self.result = min(self.result, temp) if self.result is not None else temp
+            else:
+                next_location = [location[0] + direction[0], location[1] + direction[1]]
+                if self.can_visit(next_location):
+                    temp = self.nearest_exit(maze, entrance, next_location, distance+1)
+                    self.result = min(self.result, temp) if self.result is not None else temp
+
+        return self.result if self.result is not None else -1
+
+    def is_at_exit(self, entrance):
+        return (entrance[0] == 0 or entrance[0] == len(self.maze)) or (entrance[1] == 0 or entrance[1] == len(self.maze[0]))
+
+    def can_visit(self, location):
+        return (0 <= location[0] < len(self.maze)) and (0 <= location[1] < len(self.maze[0])) and self.maze[location[0]][location[1]] == "."
