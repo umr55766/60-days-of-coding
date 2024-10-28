25fa2caf2c10fbdd2c7bfa9a3ff7d2797a9dedb4
mohammadumair <umr55766@gmail.com>
2024-10-28 13:27:10 +0400

    Fix test

--- journey_2/day8_2/solution.py
+++ journey_2/day8_2/solution.py
@@ -3,4 +3,12 @@ class Solution:
         pass
 
     def count_bits(self, n):
-        pass
+        answer = [0] * (n+1)
+
+        for number in range(1, n+1):
+            if number % 2 == 0:
+                answer[number] = answer[number//2]
+            else:
+                answer[number] = answer[number-1] + 1
+
+        return answer
