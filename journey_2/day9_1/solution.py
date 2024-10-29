1cd95fb4f7527a198445d0ecb561bcd76e3fb9b0
mohammadumair <umr55766@gmail.com>
2024-10-29 20:12:37 +0400

    Fix test 9_1

--- journey_2/day9_1/solution.py
+++ journey_2/day9_1/solution.py
@@ -2,5 +2,17 @@ class Solution:
     def __init__(self):
         pass
 
-    def daily_temperatures(self, param):
-        pass
+    def daily_temperatures(self, temperatures):
+        results = [0] * len(temperatures)
+
+        indexes_pending_update = []
+
+        for index, temperature in enumerate(temperatures):
+
+            while indexes_pending_update and temperature > temperatures[indexes_pending_update[-1]]:
+                index_pending_update = indexes_pending_update.pop()
+                results[index_pending_update] = index - index_pending_update
+
+            indexes_pending_update.append(index)
+
+        return results
