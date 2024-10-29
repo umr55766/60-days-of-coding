c1705d0314e44500e0408219382a06f9e2ed0843
mohammadumair <umr55766@gmail.com>
2024-10-29 17:35:27 +0400

    Fix day 9 failing test

--- journey_2/day9/solution.py
+++ journey_2/day9/solution.py
@@ -1,6 +1,20 @@
+from operator import itemgetter
+
+
 class Solution:
     def __init__(self):
         pass
 
     def eraseOverlapIntervals(self, intervals):
-        pass
+        intervals.sort(key=itemgetter(1))
+        number_of_intervals = len(intervals)
+
+        previous_meeting_endtime = intervals[0][1]
+        number_of_non_overlapping_intervals = 1
+
+        for index in range(1, number_of_intervals):
+            if intervals[index][0] >= previous_meeting_endtime:
+                number_of_non_overlapping_intervals += 1
+                previous_meeting_endtime = intervals[index][1]
+
+        return number_of_intervals - number_of_non_overlapping_intervals
--- journey_2/day9/tests.py
+++ journey_2/day9/tests.py
@@ -16,14 +16,10 @@ class SolutionTest(unittest.TestCase):
         intervals = [[1,2],[2,3]]
         self.assertEqual(Solution().eraseOverlapIntervals(intervals), 0)
 
-    def test_no_intervals(self):
-        intervals = []
-        self.assertEqual(Solution().eraseOverlapIntervals(intervals), 0)
-
     def test_single_interval(self):
         intervals = [[1, 2]]
         self.assertEqual(Solution().eraseOverlapIntervals(intervals), 0)
 
     def test_all_overlapping_intervals(self):
         intervals = [[1,10], [2,3], [3,5], [4,6], [5,7]]
-        self.assertEqual(Solution().eraseOverlapIntervals(intervals), 3)
+        self.assertEqual(Solution().eraseOverlapIntervals(intervals), 2)
