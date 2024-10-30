a476ef10fadc1bd8f0ae97571fb03f9fb71252ac
mohammadumair <umr55766@gmail.com>
2024-10-30 19:01:20 +0400

    Implement day 10_2

--- journey_2/day10_2/solution.py
+++ journey_2/day10_2/solution.py
@@ -3,4 +3,24 @@ class Solution:
         pass
 
     def combination_sum(self, length, aggregate):
-        pass
+
+        if length > aggregate:
+            return []
+
+        result = []
+
+        def dfs(index, combination):
+            if len(combination) == length:
+                if sum(combination) == aggregate:
+                    result.append(combination)
+                return
+
+            for digit in range(index, 10):
+                if sum(combination) + digit <= aggregate:
+                    dfs(digit+1, combination + [digit])
+                if sum(combination) + digit > aggregate:
+                    return
+
+        dfs(1, [])
+
+        return result
