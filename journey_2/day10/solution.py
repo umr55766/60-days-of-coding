4532b96d1ef32e1325b885ed87bf6e83cfb673bd
mohammadumair <umr55766@gmail.com>
2024-10-30 12:03:47 +0400

    Optimize day 10

--- journey_2/day10/solution.py
+++ journey_2/day10/solution.py
@@ -1,6 +1,13 @@
 class Solution:
     def __init__(self):
-        pass
+        self.historical_spans = []
 
-    def next(self, price):
-        pass
+    def next(self, price: int) -> int:
+        result = 1
+
+        while self.historical_spans and self.historical_spans[-1][0] <= price:
+            result += self.historical_spans.pop()[1]
+
+        self.historical_spans.append((price, result))
+
+        return result
