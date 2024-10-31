21985754544481fff0cfc0c73371b467e2467e9d
mohammadumair <umr55766@gmail.com>
2024-10-31 19:46:17 +0400

    Implement day 11_1

--- journey_2/day11_1/solution.py
+++ journey_2/day11_1/solution.py
@@ -1,3 +1,19 @@
 class Solution:
     def max_vowels(self, string, substring_length):
-        pass
+        VOWELS = ["a", "e", "i", "o", "u"]
+        if len(string) < substring_length:
+            return len([char for char in string if char in VOWELS])
+
+        substring = [char for char in string[0: substring_length]]
+        current_vowels_count = vowels_count = len([char for char in substring if char.lower() in VOWELS])
+
+        for index in range(substring_length, len(string)):
+            current_vowels_count += 1 if string[index] in VOWELS else 0
+            current_vowels_count -= 1 if substring[0] in VOWELS else 0
+
+            del substring[0]
+            substring.append(string[index])
+
+            vowels_count = max(vowels_count, current_vowels_count)
+
+        return vowels_count
