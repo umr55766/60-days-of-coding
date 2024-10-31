a8a15ece503e6582803dcb7e78e2ea51028a3f20
mohammadumair <umr55766@gmail.com>
2024-10-31 15:09:37 +0400

    solve day 11

--- journey_2/day11/solution.py
+++ journey_2/day11/solution.py
@@ -1,3 +1,18 @@
 class Solution:
-    def is_subsequence(self, param, param1):
-        pass
+    def is_subsequence(self, subsequence: str, main_string: str) -> bool:
+        if len(subsequence) == 0:
+            return True
+
+        if len(main_string) == 0:
+            return False
+
+        subsequence_index = 0
+
+        for char in main_string:
+            if char == subsequence[subsequence_index]:
+                subsequence_index += 1
+
+            if subsequence_index == len(subsequence) and subsequence_index != 0:
+                return True
+
+        return False
