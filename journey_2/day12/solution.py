8f95a7c6a1f6b7f0448eda05929851a76a864a78
mohammadumair <umr55766@gmail.com>
2024-11-02 22:10:22 +0400

    Implement day 12

--- journey_2/day12/solution.py
+++ journey_2/day12/solution.py
@@ -2,5 +2,13 @@ class Solution:
     def __init__(self):
         pass
 
-    def get_pivot_point(self, param):
-        return param
+    def get_pivot_point(self, nums):
+        total_sum = sum(nums)
+        left_sum = 0
+
+        for index, num in enumerate(nums):
+            if left_sum == total_sum - left_sum - num:
+                return index
+            left_sum += num
+
+        return -1
