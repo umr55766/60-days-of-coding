4a43d19ed9ec57db7f4243027a55739307a339fd
mohammadumair <umr55766@gmail.com>
2024-11-02 23:19:00 +0400

    Implemented day_11_1

--- journey_2/day12_1/solution.py
+++ journey_2/day12_1/solution.py
@@ -2,5 +2,13 @@ class Solution:
     def __init__(self):
         pass
 
-    def is_occurences_unique(self, param):
-        return param
+    def is_occurences_unique(self, numbers):
+        number_count_dictionary = {}
+
+        for number in numbers:
+            if number in number_count_dictionary:
+                number_count_dictionary[number] += 1
+            else:
+                number_count_dictionary[number] = 1
+
+        return len(number_count_dictionary.values()) == len(set(number_count_dictionary.values()))
