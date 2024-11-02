a61e4d2baab9a1ff94ed72d1bc097a03429a39f1
mohammadumair <umr55766@gmail.com>
2024-11-02 22:08:30 +0400

    Add day 12

---
+++ journey_2/day12/tests.py
@@ -0,0 +1,14 @@
+import unittest
+
+from journey_2.day12.solution import Solution
+
+
+class SolutionTest(unittest.TestCase):
+    def test_1(self):
+        self.assertEqual(Solution().method_name(1), 1)
+
+    def test_2(self):
+        self.assertEqual(Solution().method_name(2), 2)
+
+    def test_3(self):
+        self.assertEqual(Solution().method_name(3), 3)
