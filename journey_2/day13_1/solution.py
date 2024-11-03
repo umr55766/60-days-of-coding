9e04238446f90e44e79454c19b5089c95276e7e5
mohammadumair <umr55766@gmail.com>
2024-11-03 11:45:56 +0400

    Optimize day 13_1 get_longest_subsequence_length

--- journey_2/day13_1/solution.py
+++ journey_2/day13_1/solution.py
@@ -3,13 +3,19 @@ class Solution:
         pass
 
     def get_longest_subsequence_length(self, sequence1, sequence2):
-        lengths = [[0] * (len(sequence2)+1) for _ in range(len(sequence1)+1)]
+        if len(sequence1) < len(sequence2):
+            sequence1, sequence2 = sequence2, sequence1
 
-        for index1 in range(1, len(sequence1)+1):
-            for index2 in range(1, len(sequence2)+1):
-                if sequence1[index1-1] == sequence2[index2-1]:
-                    lengths[index1][index2] = lengths[index1-1][index2-1] + 1
+        previous_row = [0] * (len(sequence2) + 1)
+        current_row = [0] * (len(sequence2) + 1)
+
+        for index1 in range(1, len(sequence1) + 1):
+            for index2 in range(1, len(sequence2) + 1):
+                if sequence1[index1 - 1] == sequence2[index2 - 1]:
+                    current_row[index2] = previous_row[index2 - 1] + 1
                 else:
-                    lengths[index1][index2] = max(lengths[index1 - 1][index2], lengths[index1][index2 - 1])
+                    current_row[index2] = max(previous_row[index2], current_row[index2 - 1])
 
-        return lengths[len(sequence1)][len(sequence2)]
+            previous_row, current_row = current_row, previous_row
+
+        return previous_row[len(sequence2)]
