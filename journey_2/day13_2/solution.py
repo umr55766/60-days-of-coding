931c7b3cbf9d410efb6aae2a7a0223a6a9194d89
mohammadumair <umr55766@gmail.com>
2024-11-03 12:02:01 +0400

    Solve day 13_2 minimum_cost_stairs

--- journey_2/day13_2/solution.py
+++ journey_2/day13_2/solution.py
@@ -2,5 +2,11 @@ class Solution:
     def __init__(self):
         pass
 
-    def minimum_cost_stairs(self, param):
-        return param
+    def minimum_cost_stairs(self, stairs_cost):
+        second_last_stair = stairs_cost[0]
+        last_stair = stairs_cost[1]
+
+        for index in range(2, len(stairs_cost)):
+            second_last_stair, last_stair = last_stair, min(second_last_stair, last_stair) + stairs_cost[index]
+
+        return min(last_stair, second_last_stair)
