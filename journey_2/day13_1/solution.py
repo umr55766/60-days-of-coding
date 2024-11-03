ec3ac753e94588134323c7501fb9aa8ddafcb10d
mohammadumair <umr55766@gmail.com>
2024-11-03 11:16:40 +0400

    Add day 13_1 get_longest_subsequence_length

---
+++ journey_2/day13_1/solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def __init__(self):
+        pass
+
+    def get_longest_subsequence_length(self, param, s):
+        return param
---
+++ journey_2/day13_1/tests.py
@@ -0,0 +1,14 @@
+import unittest
+
+from journey_2.day13_1.solution import Solution
+
+
+class SolutionTest(unittest.TestCase):
+    def test_1(self):
+        self.assertEqual(Solution().get_longest_subsequence_length("abcde", "ace"), 3)
+
+    def test_2(self):
+        self.assertEqual(Solution().get_longest_subsequence_length("abc", "abc"), 3)
+
+    def test_3(self):
+        self.assertEqual(Solution().get_longest_subsequence_length("abc", "def"), 0)
