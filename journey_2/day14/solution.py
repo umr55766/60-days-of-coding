368a414e3491c1e6966e0e4e9b9f1c7fd34ab2f7
mohammadumair <umr55766@gmail.com>
2024-11-04 15:49:05 +0400

    solve day 14 reverse_vowels

--- journey_2/day14/solution.py
+++ journey_2/day14/solution.py
@@ -3,4 +3,18 @@ class Solution:
         pass
 
     def reverse_vowels(self, string):
-        return string
+        string = list(string)
+        low, high = 0, len(string) - 1
+
+        while low < high:
+            while low < high and string[low] not in {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}:
+                low += 1
+
+            while low < high and string[high] not in {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}:
+                high -= 1
+
+            string[low], string[high] = string[high], string[low]
+            low += 1
+            high -= 1
+
+        return "".join(string)
