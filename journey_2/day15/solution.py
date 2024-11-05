cb13886754fe41d4987dd8fd38e3c8e5214c0010
mohammadumair <umr55766@gmail.com>
2024-11-05 19:58:28 +0400

    Solve day 14 predict_victory_party

--- journey_2/day15/solution.py
+++ journey_2/day15/solution.py
@@ -1,6 +1,27 @@
+from collections import deque
+
+
 class Solution:
     def __init__(self):
         pass
 
-    def predict_victory_party(self, param):
-        return param
+    def predict_victory_party(self, senate):
+        radiant_queue = deque()
+        dire_queue = deque()
+
+        for index, senator in enumerate(senate):
+            if senator == "R":
+                radiant_queue.append(index)
+            else:
+                dire_queue.append(index)
+
+        while radiant_queue and dire_queue:
+            radiant = radiant_queue.popleft()
+            dire = dire_queue.popleft()
+
+            if radiant < dire:
+                radiant_queue.append(radiant + len(senate))
+            else:
+                dire_queue.append(dire + len(senate))
+
+        return "Radiant" if radiant_queue else "Dire"
