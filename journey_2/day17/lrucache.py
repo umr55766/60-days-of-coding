464fe92bd135119f84aa9e71d3d463e9ae76e3bc
mohammadumair <umr55766@gmail.com>
2024-11-10 13:27:07 +0400

    Add day 17 LRU Cache

---
+++ journey_2/day17/lrucache.py
@@ -0,0 +1,13 @@
+from collections import OrderedDict
+
+
+class LRUCache:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.cache = OrderedDict()
+
+    def put(self, key, value):
+        pass
+
+    def get(self, key):
+        pass
---
+++ journey_2/day17/tests.py
@@ -0,0 +1,17 @@
+import unittest
+
+from journey_2.day17.lrucache import LRUCache
+
+
+class SolutionTest(unittest.TestCase):
+    def test(self):
+        cache = LRUCache(2)
+        cache.put(1, 1)
+        cache.put(2, 2)
+        self.assertEqual(cache.get(1), 1)
+        cache.put(3, 3)
+        self.assertEqual(cache.get(2), -1)
+        cache.put(4, 4)
+        self.assertEqual(cache.get(1), -1)
+        self.assertEqual(cache.get(3), 3)
+        self.assertEqual(cache.get(4), 4)
