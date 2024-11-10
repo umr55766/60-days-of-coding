58c007223dc7bc909423798583ec180923735eda
mohammadumair <umr55766@gmail.com>
2024-11-10 13:28:49 +0400

    Implement LRU Get

--- journey_2/day17/lrucache.py
+++ journey_2/day17/lrucache.py
@@ -2,6 +2,7 @@ from collections import OrderedDict
 
 
 class LRUCache:
+    NOT_FOUND = -1
     def __init__(self, capacity):
         self.capacity = capacity
         self.cache = OrderedDict()
@@ -10,4 +11,9 @@ class LRUCache:
         pass
 
     def get(self, key):
-        pass
+        if key not in self.cache:
+            return self.NOT_FOUND
+
+        self.cache.move_to_end(key)
+
+        return self.cache[key]
