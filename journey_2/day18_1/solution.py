7fd23dee577fa4c8c0db677330415e52d2e0e613
mohammadumair <umr55766@gmail.com>
2024-11-11 17:18:44 +0400

    Solve day 18_1 have similar leaf

--- journey_2/day18_1/solution.py
+++ journey_2/day18_1/solution.py
@@ -1,3 +1,8 @@
+from itertools import zip_longest
+
+from setuptools.dist import sequence
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -10,4 +15,15 @@ class Solution:
         pass
 
     def have_similar_leaf(self, root1, root2):
-        return root1
+        def leaf_generator(node):
+            if node:
+                if not node.left and not node.right:
+                    yield node.val
+                else:
+                    yield from leaf_generator(node.left)
+                    yield from leaf_generator(node.right)
+
+        for val1, val2 in zip_longest(leaf_generator(root1), leaf_generator(root2), fillvalue=None):
+            if val1 != val2:
+                return False
+        return True
