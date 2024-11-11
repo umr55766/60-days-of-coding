30b3b162fbc262d45d62267a964a0b7516544f53
mohammadumair <umr55766@gmail.com>
2024-11-11 18:16:34 +0400

    Solve day 18_2 max level sum

--- journey_2/day18_2/solution.py
+++ journey_2/day18_2/solution.py
@@ -10,4 +10,28 @@ class Solution:
         pass
 
     def max_level_sum(self, root):
-        pass
+        level_sums = self.get_all_level_sums(root, {}, 1)
+
+        max_sum = level_sums[1]
+        max_sum_level = 1
+
+        for key in level_sums:
+            if level_sums[key] > max_sum:
+                max_sum = level_sums[key]
+                max_sum_level = key
+
+        return max_sum_level
+
+    def get_all_level_sums(self, root: TreeNode, level_sums : dict, level: int):
+        if level in level_sums:
+            level_sums[level] += root.val
+        else:
+            level_sums[level] = root.val
+
+        if root.left:
+            self.get_all_level_sums(root.left, level_sums, level + 1)
+
+        if root.right:
+            self.get_all_level_sums(root.right, level_sums, level + 1)
+
+        return level_sums
